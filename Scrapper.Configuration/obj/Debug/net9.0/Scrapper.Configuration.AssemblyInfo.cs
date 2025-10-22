8ccac017323384314c9d61683365f4961ac06419
Brian Zabandith <brian.zabandith@assurware.com>
2025-10-23 00:50:22 +0200

    update dll files

--- Scrapper.Configuration/obj/Debug/net9.0/Scrapper.Configuration.AssemblyInfo.cs
+++ Scrapper.Configuration/obj/Debug/net9.0/Scrapper.Configuration.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Scrapper.Configuration")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78de7f5241321af41352645b3b2fbb76ad43556b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f10d569d72f8af660d32375058e87117a8f6d4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Scrapper.Configuration")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Scrapper.Configuration")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
